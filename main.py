b6b5c653cf7f6810ded22683d78aac9546a5e253
trexxoficial <trexxoficialco@gmail.com>
2025-06-25 20:18:46 -0500

    c

--- main.py
+++ main.py
@@ -9,31 +9,30 @@ app = FastAPI()
 
 CSV_FILE_PATH = "https://firebasestorage.googleapis.com/v0/b/davgui24-6182c.firebasestorage.app/o/analisis_de_datos%2FViolencia_clean.csv?alt=media&token=664ebff8-6ba3-403d-92f0-16ee795ab344"
 
-async def cargar_csv_async():
+async def get_column_names():
     async with httpx.AsyncClient() as client:
-        resp = await client.get(CSV_FILE_PATH)
-        df = pd.read_csv(StringIO(resp.text), sep=";")
-        return df
-# df = pd.read_csv(CSV_FILE_PATH, sep=";") 
-def get_column_names():
-    
-    resumen = {
-        "filas": cargar_csv_async().shape[0],
-        "columnas": cargar_csv_async().shape[1],
-        "columnas_info": cargar_csv_async().dtypes.astype(str).to_dict(),
-    }
-    return resumen
+        response = await client.get(CSV_FILE_PATH, timeout=30.0)
+        response.raise_for_status()  # lanza excepción si hay error HTTP
+        csv_data = StringIO(response.text)
+        df = pd.read_csv(csv_data, sep=";")
 
+        resumen = {
+            "filas": df.shape[0],
+            "columnas": df.shape[1],
+            "columnas_info": df.dtypes.astype(str).to_dict(),
+        }
+        return resumen
 
 @app.get("/")
 def root():
-    return {"message": "API de FastAPI desplegada en Render API. Visita /variables para ver los datos."}
-
+    return {
+        "message": "API de FastAPI en Render funcionando en V 2. Visita /variables para ver las columnas del CSV."
+    }
 
 @app.get("/variables")
-def variables():
-    # Leer el archivo CSV
-    return {"data": get_column_names()}
+async def variables():
+    resumen = await get_column_names()
+    return {"data": resumen}
 # def get_variables(columns: str = None):
 #     try:
 #         # Leer el archivo CSV
