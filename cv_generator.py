5451d38465958f24c64ad8f65794f8de812aac5a
davgui24 <davgui242011@gmail.com>
2026-02-13 18:27:19 -0500

    creacion de perfil

--- cv_generator.py
+++ cv_generator.py
@@ -1,22 +1,17 @@
+# cv_generator.py
+import os
 from docxtpl import DocxTemplate
 import io
-import os
 
 def crear_docx_cv(data):
-    # 1. Obtiene la ruta de la carpeta donde está ESTE archivo (cv_generator.py)
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    
-    # 2. Une esa ruta con el nombre de tu plantilla
-    template_path = os.path.join(base_dir, "plantillas/plantilla_cv.docx")
-    
-    print(f"Buscando plantilla en: {template_path}")  # Esto se imprimirá en tu terminal
+    # ACTUALIZACIÓN AQUÍ: Carpeta 'plantillas', archivo 'plantilla_cv.docx'
+    template_path = os.path.join(base_dir, "plantillas", "plantilla_cv.docx")
 
     if not os.path.exists(template_path):
-        raise FileNotFoundError(f"¡ERROR CRÍTICO! No encuentro el archivo en: {template_path}")
+        raise FileNotFoundError(f"No encuentro la plantilla en: {template_path}")
 
     doc = DocxTemplate(template_path)
-    
-    # Renderizamos
     doc.render(data)
     
     file_stream = io.BytesIO()
