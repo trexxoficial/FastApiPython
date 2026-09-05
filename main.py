6adad1fd76156938aa56e0793ab2f1f7c7312864
davgui24 <davgui242011@gmail.com>
2026-09-05 18:06:17 -0500

    c

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
